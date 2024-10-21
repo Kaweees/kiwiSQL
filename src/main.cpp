df3b2a92c86350e8f56b230de7a4183c5ac506a3
Miguel Villa Floran <miguel.villafloran@gmail.com>
2024-10-21 14:11:33 -0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <stddef.h>
+#include <stdio.h>
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main(int argc, char **argv) { printf("Hello World!\n"); }
